561af9354766faec21e7cb38a603f754e0148ee6
gyubhin <iami0115@naver.com>
2025-08-22 21:23:37 +0900

    @BeforeEach 설정

--- src/test/java/myspring/di/xml/HelloJUnit5Test.java
+++ src/test/java/myspring/di/xml/HelloJUnit5Test.java
@@ -3,15 +3,22 @@ package myspring.di.xml;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
+import org.junit.jupiter.api.BeforeEach;
 
 //static import
 import static org.junit.jupiter.api.Assertions.*;
 
 public class HelloJUnit5Test {
+	ApplicationContext context;
+	
+	@BeforeEach
+	void createContainer() {
+		//1. Container 객체 생성
+		context = new GenericXmlApplicationContext("classpath:spring-beans.xml");
+	}
+
 	@Test
 	void helloBean() {
-		//1. Container 객체 생성
-		ApplicationContext context = new GenericXmlApplicationContext("classpath:spring-beans.xml");
 		//2. Container가 생성한 Hello 스프링빈을 요청하기
 		Hello helloById = (Hello)context.getBean("hello");
 		Hello helloByType = context.getBean("hello", Hello.class);
