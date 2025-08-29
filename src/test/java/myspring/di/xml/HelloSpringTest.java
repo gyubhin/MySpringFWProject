daa51dd87e05bc37204ed71e27c53d5af32dad4f
gyubhin <iami0115@naver.com>
2025-08-29 11:02:35 +0900

    Spring test Annotation

---
+++ src/test/java/myspring/di/xml/HelloSpringTest.java
@@ -0,0 +1,25 @@
+package myspring.di.xml;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.junit.jupiter.api.Test;
+//static import
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(locations = "classpath:spring-beans.xml")
+public class HelloSpringTest {
+	@Autowired
+	@Qualifier("helloC")
+	Hello hello;
+	
+	@Test
+	void helloBeanByConstructor() {
+		System.out.println(hello.sayHello());
+		assertEquals("Hello 생성자", hello.sayHello());
+		
+	}
+}
